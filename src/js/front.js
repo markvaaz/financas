5319194eee666e80420577b19e77565bcefc339e
markvaaz <marcosrodolfo32@gmail.com>
2020-01-22 19:01:04 +0000

    0

--- src/js/front.js
+++ src/js/front.js
@@ -2,7 +2,7 @@ import {calculos as calculos} from "./index.js";
 import {create as create} from "./tools.js";
 import {card as card} from "./cards.js";
 window.card = card;
-
+console.log(card)
 let load = {
   resumo:function(){
     let rl = document.getElementById("rl");
