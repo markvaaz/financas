76938089ac0459399ddd6f01021d7ec424a45aae
markvaaz <marcosrodolfo32@gmail.com>
2020-01-22 14:13:15 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,10 @@
 
 //let config = require("./config");
-
 import {config as config} from "./config.js";
+import {card as card} from "./cards.js";
 window.config = config;
+window.card = card;
 
-console.log(config);
-
-let card = [
-  {
-    descricao:"",
-    valor:1500,
-    parcelas:10,
-    parcelasPagas:0,
-    vencimento:0,
-    juros:0
-  },
-  {
-    descricao:"",
-    valor:2595,
-    parcelas:6,
-    parcelasPagas:3,
-    vencimento:0,
-    juros:0.5
-  }
-]
 let calculos = {
   salario:function(){
     let resultado = 0
@@ -52,16 +33,21 @@ let calculos = {
     return item.toLocaleString('pt-BR', {minimumFractionDigits: 2,  maximumFractionDigits: 2, style: 'currency', currency: 'BRL'});
   }
 }
-function load(){
-  let rl = document.getElementById("rl");
-  let sm = document.getElementById("sm");
-  let dm = document.getElementById("dm");
-  let dt = document.getElementById("dt");
-  
-  rl.innerHTML = calculos.converter(config.renda);
-  sm.innerHTML = calculos.salario();
-  dm.innerHTML = calculos.dividaMensal();
-  dt.innerHTML = calculos.divida();
+let load = {
+  resumo:function(){
+    let rl = document.getElementById("rl");
+    let sm = document.getElementById("sm");
+    let dm = document.getElementById("dm");
+    let dt = document.getElementById("dt");
+    
+    rl.innerHTML = calculos.converter(config.renda);
+    sm.innerHTML = calculos.salario();
+    dm.innerHTML = calculos.dividaMensal();
+    dt.innerHTML = calculos.divida();
+  },
+  cards:function(){
+    
+  }
 }
 console.log(calculos.salario());
 window.onload = load();
