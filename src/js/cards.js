0c4d39626b32c3b418dec163fd6672a519673abb
markvaaz <marcosrodolfo32@gmail.com>
2020-01-22 14:13:40 -0300

    Update cards.js

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,1 +1,20 @@
-// Aqui ficará as funções de renderização de cards na pagina
+let card = [
+  {
+    descricao:"",
+    valor:1500,
+    parcelas:10,
+    parcelasPagas:0,
+    vencimento:0,
+    juros:0
+  },
+  {
+    descricao:"",
+    valor:2595,
+    parcelas:6,
+    parcelasPagas:3,
+    vencimento:0,
+    juros:0.5
+  }
+]
+
+export {card as card};
