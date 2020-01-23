0c145a39a1389c25cc2e7decb0507651650399d8
markvaaz <marcosrodolfo32@gmail.com>
2020-01-23 12:22:48 +0000

    0

--- src/js/ferramentas.js
+++ src/js/ferramentas.js
@@ -20,32 +20,4 @@ function create(elementName, attributes){
   }
   return element;
 }
-let calculos = {
-  salario:function(){
-    let resultado = 0
-    for(let i = 0; i < card.length; i++){ 
-      if(card[i].parcelas > card[i].parcelasPagas){
-        resultado += card[i].valor / card[i].parcelas;
-      }
-    }
-    return calculos.converter((config.renda - resultado) + config.rendaExtra);
-  },
-  dividaMensal:function(){
-    let resultado = 0
-    for(let i = 0; i < card.length; i++) resultado += card[i].valor / card[i].parcelas;
-    return calculos.converter(resultado);
-  },
-  divida:function(){
-    let resultado = 0;
-    for(let i = 0; i < card.length; i++) resultado += card[i].valor - ((card[i].valor / card[i].parcelas) * card[i].parcelasPagas);
-    return calculos.converter(resultado);
-  },
-  juros:function(item){
-    let parcelas = item.valor / item.parcelas;
-    return calculos.converter((parcelas * (item.juros / 100)).toFixed(2))
-  },
-  converter:function(item){
-    return item.toLocaleString('pt-BR', {minimumFractionDigits: 2,  maximumFractionDigits: 2, style: 'currency', currency: 'BRL'});
-  }
-}
-export {create, calculos}
+export {create}
