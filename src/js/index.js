a3a8399ea62800db946921e4813636db497deadd
markvaaz <marcosrodolfo32@gmail.com>
2020-01-23 18:35:01 +0000

    0

--- src/js/index.js
+++ src/js/index.js
@@ -33,6 +33,18 @@ let load = {
       for(let i = 0; i < card.length; i++){
         if(card[i].vencimento >= data.dia){
           resultado = true;
+          let novoCard = create("div", {className:"card shadow", appendChild:[
+            create("div", {className:"descricao", innerHTML:`<i class="fas fa-grip-horizontal"></i> <span>${card[i].descricao}</span>`}),
+            create("div", {className:"valor", innerHTML:`Valor <span>${calculos.converter(card[i].valor)}</span>`}),
+            create("div", {className:"proxima-parcela", innerHTML:`Proxima parcela <span>${calculos.converter(card[i].valor / card[i].parcelas)}</span>`}),
+            create("div", {className:"valor-pago", innerHTML:`Valor pago <span>${calculos.converter(card[i].parcelasPagas !== 0 ? ((card[i].valor / card[i].parcelas) * card[i].parcelasPagas) : 0)}</span>`}),
+            create("div", {className:"parcelas", innerHTML:`Parcelas <span>${card[i].parcelas}</span>`}),
+            create("div", {className:"parcelas-pagas", innerHTML:`Parcelas pagas <span>${card[i].parcelasPagas}</span>`}),
+            create("div", {className:"vencimento", innerHTML:`Dia de vencimento <span>${card[i].vencimento}</span>`}),
+            create("div", {className:"juros", innerHTML:`Juros <span>${card[i].juros} %</span>`}),
+            create("div", {className:"historico", innerHTML:`Historico <span>${card[i].historico !== null ? card[i].historico : "Indisponível"}</span>`})
+          ]});
+          cardContainer.appendChild(novoCard);
         }
       }
     }
@@ -42,18 +54,7 @@ let load = {
       titleVencimentos.innerHTML = "Proximos vencimentos em "+data.mes(1);
     }
     for(let i = 0; i < card.length; i++){
-      let novoCard = create("div", {className:"card shadow", appendChild:[
-        create("div", {className:"descricao", innerHTML:`<i class="fas fa-grip-horizontal"></i> <span>${card[i].descricao}</span>`}),
-        create("div", {className:"valor", innerHTML:`Valor <span>${calculos.converter(card[i].valor)}</span>`}),
-        create("div", {className:"proxima-parcela", innerHTML:`Proxima parcela <span>${calculos.converter(card[i].valor / card[i].parcelas)}</span>`}),
-        create("div", {className:"valor-pago", innerHTML:`Valor pago <span>${calculos.converter(card[i].parcelasPagas !== 0 ? ((card[i].valor / card[i].parcelas) * card[i].parcelasPagas) : 0)}</span>`}),
-        create("div", {className:"parcelas", innerHTML:`Parcelas <span>${card[i].parcelas}</span>`}),
-        create("div", {className:"parcelas-pagas", innerHTML:`Parcelas pagas <span>${card[i].parcelasPagas}</span>`}),
-        create("div", {className:"vencimento", innerHTML:`Dia de vencimento <span>${card[i].vencimento}</span>`}),
-        create("div", {className:"juros", innerHTML:`Juros <span>${card[i].juros} %</span>`}),
-        create("div", {className:"historico", innerHTML:`Historico <span>${card[i].historico !== null ? card[i].historico : "Indisponível"}</span>`})
-      ]});
-      cardContainer.appendChild(novoCard);
+      
     }
   }
 }
