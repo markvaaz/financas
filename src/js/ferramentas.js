0e86093151a19369533eedc86d893adec7e77db6
markvaaz <marcosrodolfo32@gmail.com>
2020-01-23 17:53:31 +0000

    0

--- src/js/ferramentas.js
+++ src/js/ferramentas.js
@@ -33,21 +33,21 @@ let tools = {
           resultado += calc[i].valor / calc[i].parcelas;
         }
       }
-      return calculos.converter((config.renda - resultado) + config.rendaExtra);
+      return this.calculos.converter((config.renda - resultado) + config.rendaExtra);
     },
     dividaMensal:function(calc){
       let resultado = 0
       for(let i = 0; i < calc.length; i++) resultado += calc[i].valor / calc[i].parcelas;
-      return calculos.converter(resultado);
+      return this.calculos.converter(resultado);
     },
     dividaTotal:function(calc){
       let resultado = 0;
       for(let i = 0; i < calc.length; i++) resultado += calc[i].valor - ((calc[i].valor / calc[i].parcelas) * calc[i].parcelasPagas);
-      return calculos.converter(resultado);
+      return this.calculos.converter(resultado);
     },
     juros:function(item){
       let parcelas = item.valor / item.parcelas;
-      return calculos.converter((parcelas * (item.juros / 100)).toFixed(2))
+      return this.calculos.converter((parcelas * (item.juros / 100)).toFixed(2))
     },
     converter:function(item){
       return item.toLocaleString('pt-BR', {minimumFractionDigits: 2,  maximumFractionDigits: 2, style: 'currency', currency: 'BRL'});
