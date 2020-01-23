b93b11b3a9fe0037dc6d7114da0650948001d420
markvaaz <marcosrodolfo32@gmail.com>
2020-01-23 11:43:22 +0000

    0

--- src/js/front.js
+++ src/js/front.js
@@ -1,8 +1,35 @@
-import {calculos as calculos} from "./index.js";
 import {create as create} from "./tools.js";
 import {card as card} from "./cards.js";
-window.card = card;
-console.log(card);
+import {config as config} from "./config.js";
+
+let calculos = {
+  salario:function(){
+    let resultado = 0
+    for(let i = 0; i < card.length; i++){ 
+      if(card[i].parcelas > card[i].parcelasPagas){
+        resultado += card[i].valor / card[i].parcelas;
+      }
+    }
+    return calculos.converter((config.renda - resultado) + config.rendaExtra);
+  },
+  dividaMensal:function(){
+    let resultado = 0
+    for(let i = 0; i < card.length; i++) resultado += card[i].valor / card[i].parcelas;
+    return calculos.converter(resultado);
+  },
+  divida:function(){
+    let resultado = 0;
+    for(let i = 0; i < card.length; i++) resultado += card[i].valor - ((card[i].valor / card[i].parcelas) * card[i].parcelasPagas);
+    return calculos.converter(resultado);
+  },
+  juros:function(item){
+    let parcelas = item.valor / item.parcelas;
+    return calculos.converter((parcelas * (item.juros / 100)).toFixed(2))
+  },
+  converter:function(item){
+    return item.toLocaleString('pt-BR', {minimumFractionDigits: 2,  maximumFractionDigits: 2, style: 'currency', currency: 'BRL'});
+  }
+}
 
 let load = {
   resumo:function(){
@@ -34,4 +61,4 @@ let load = {
     }
   }
 }
-export {load as load};
+export {load as load, calculos as calculos};
--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,6 @@
 
 //let config = require("./config");
-import {config as config} from "./config.js";
-import {card as card} from "./cards.js";
 import {load as load} from "./front.js";
-import {create as create} from "./tools.js";
 
-window.config = config;
-window.card = card;
-window.load = load;
-window.create = create;
-
-let calculos = {
-  salario:function(){
-    let resultado = 0
-    for(let i = 0; i < card.length; i++){ 
-      if(card[i].parcelas > card[i].parcelasPagas){
-        resultado += card[i].valor / card[i].parcelas;
-      }
-    }
-    return calculos.converter((config.renda - resultado) + config.rendaExtra);
-  },
-  dividaMensal:function(){
-    let resultado = 0
-    for(let i = 0; i < card.length; i++) resultado += card[i].valor / card[i].parcelas;
-    return calculos.converter(resultado);
-  },
-  divida:function(){
-    let resultado = 0;
-    for(let i = 0; i < card.length; i++) resultado += card[i].valor - ((card[i].valor / card[i].parcelas) * card[i].parcelasPagas);
-    return calculos.converter(resultado);
-  },
-  juros:function(item){
-    let parcelas = item.valor / item.parcelas;
-    return calculos.converter((parcelas * (item.juros / 100)).toFixed(2))
-  },
-  converter:function(item){
-    return item.toLocaleString('pt-BR', {minimumFractionDigits: 2,  maximumFractionDigits: 2, style: 'currency', currency: 'BRL'});
-  }
-}
 load.resumo();
 load.cards();
-export {calculos as calculos}
